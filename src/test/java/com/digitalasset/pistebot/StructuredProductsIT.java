8133880664238a9977742c4dd762c2213517e24c
gaborh-da <33900822+gaborh-da@users.noreply.github.com>
2019-08-06 16:17:53 +0200

    Upgrade to Func Test Lib 0.1.5 (#23)

--- src/test/java/com/digitalasset/pistebot/StructuredProductsIT.java
+++ src/test/java/com/digitalasset/pistebot/StructuredProductsIT.java
@@ -11,8 +11,10 @@ import com.daml.ledger.javaapi.data.Identifier;
 import com.daml.ledger.javaapi.data.Party;
 import com.daml.ledger.javaapi.data.Value;
 import com.digitalasset.testing.junit4.Sandbox;
+import com.digitalasset.testing.ledger.DefaultLedgerAdapter;
 import com.digitalasset.testing.utils.ContractWithId;
 import com.google.common.collect.Lists;
+import com.google.protobuf.InvalidProtocolBufferException;
 import da.refapps.structuredproducts.dcn.*;
 import java.math.BigDecimal;
 import java.nio.file.Path;
@@ -50,9 +52,7 @@ public class StructuredProductsIT {
           .scenario(TEST_SCENARIO)
           .parties(INTERMEDIARY_PARTY.getValue(), CLIENT_PARTY.getValue(), ISSUER_PARTY.getValue())
           .setupAppCallback(
-              client ->
-                  Main.runBots(
-                      "localhost", sandboxPort, "./output_messages", telegramMessages::add))
+              client -> Main.runBots(client, "./output_messages", telegramMessages::add))
           .build();
 
   @ClassRule public static ExternalResource compile = sandboxC.compilation();
@@ -64,48 +64,43 @@ public class StructuredProductsIT {
   }
 
   @Test
-  public void testWorkflow() {
+  public void testWorkflow() throws InvalidProtocolBufferException {
+    final DefaultLedgerAdapter ledgerAdapter = sandbox.getLedgerAdapter();
     TradeProposal.ContractId tradeProposal =
-        sandbox.getCreatedContractId(
+        ledgerAdapter.getCreatedContractId(
             INTERMEDIARY_PARTY, TradeProposal.TEMPLATE_ID, TradeProposal.ContractId::new);
-    sandbox
-        .getLedgerAdapter()
-        .exerciseChoice(
-            INTERMEDIARY_PARTY, tradeProposal.exerciseAccept("INTER001", "INTXXXABC", "1234567"));
+    ledgerAdapter.exerciseChoice(
+        INTERMEDIARY_PARTY, tradeProposal.exerciseAccept("INTER001", "INTXXXABC", "1234567"));
     Trade.ContractId tradeCid =
-        sandbox.getCreatedContractId(INTERMEDIARY_PARTY, Trade.TEMPLATE_ID, Trade.ContractId::new);
-    sandbox
-        .getLedgerAdapter()
-        .exerciseChoice(
-            INTERMEDIARY_PARTY,
-            tradeCid.exerciseProposeTradeToClient(
-                CLIENT_PARTY.getValue(), BigDecimal.valueOf(26000000)));
+        ledgerAdapter.getCreatedContractId(
+            INTERMEDIARY_PARTY, Trade.TEMPLATE_ID, Trade.ContractId::new);
+    ledgerAdapter.exerciseChoice(
+        INTERMEDIARY_PARTY,
+        tradeCid.exerciseProposeTradeToClient(
+            CLIENT_PARTY.getValue(), BigDecimal.valueOf(26000000)));
 
     // Check the trade proposal with the client.
     ContractWithId<TradeProposal.ContractId> clientTrade =
-        sandbox.getMatchedContract(
+        ledgerAdapter.getMatchedContract(
             CLIENT_PARTY, TradeProposal.TEMPLATE_ID, TradeProposal.ContractId::new);
     assertEquals(
         26000000L,
         TradeProposal.fromValue(clientTrade.record).salesPrice.toBigInteger().longValue());
-    sandbox
-        .getLedgerAdapter()
-        .exerciseChoice(
-            CLIENT_PARTY,
-            clientTrade.contractId.exerciseAccept("CLIENT001", "CLIXXXABC", "5678910"));
+    ledgerAdapter.exerciseChoice(
+        CLIENT_PARTY, clientTrade.contractId.exerciseAccept("CLIENT001", "CLIXXXABC", "5678910"));
 
     Trade.ContractId clientTradeCid =
-        sandbox.getCreatedContractId(INTERMEDIARY_PARTY, Trade.TEMPLATE_ID, Trade.ContractId::new);
+        ledgerAdapter.getCreatedContractId(
+            INTERMEDIARY_PARTY, Trade.TEMPLATE_ID, Trade.ContractId::new);
 
     LocalDate date1 = LocalDate.of(2019, 11, 11);
     LocalDate date2 = LocalDate.of(2022, 2, 8);
-    sandbox
-        .getLedgerAdapter()
-        .setCurrentTime(Instant.ofEpochSecond(date1.toEpochDay() * 24 * 60 * 60));
+    ledgerAdapter.setCurrentTime(Instant.ofEpochSecond(date1.toEpochDay() * 24 * 60 * 60));
 
     // get market data
     MarketData.ContractId marketData2019Cid =
         find(
+            ledgerAdapter,
             ISSUER_PARTY,
             MarketData.TEMPLATE_ID,
             MarketData.ContractId::new,
@@ -113,6 +108,7 @@ public class StructuredProductsIT {
             m -> m.publishDate.atZone(ZoneOffset.UTC).toLocalDate().equals(date1));
     MarketData.ContractId marketData2022Cid =
         find(
+            ledgerAdapter,
             ISSUER_PARTY,
             MarketData.TEMPLATE_ID,
             MarketData.ContractId::new,
@@ -120,30 +116,25 @@ public class StructuredProductsIT {
             m -> m.publishDate.atZone(ZoneOffset.UTC).toLocalDate().equals(date2));
 
     // Coupon Event: Issuer vs. Intermediary
-    sandbox
-        .getLedgerAdapter()
-        .exerciseChoice(ISSUER_PARTY, tradeCid.exerciseLifecycle(marketData2019Cid));
+    ledgerAdapter.exerciseChoice(ISSUER_PARTY, tradeCid.exerciseLifecycle(marketData2019Cid));
     // check if event created
-    sandbox.getCreatedContractId(
+    ledgerAdapter.getCreatedContractId(
         ISSUER_PARTY, CouponEvent.TEMPLATE_ID, CouponEvent.ContractId::new);
 
     // Coupon Event: Intermediary vs. Client
-    sandbox
-        .getLedgerAdapter()
-        .exerciseChoice(INTERMEDIARY_PARTY, clientTradeCid.exerciseLifecycle(marketData2019Cid));
-    sandbox
-        .getLedgerAdapter()
-        .setCurrentTime(Instant.ofEpochSecond(date2.toEpochDay() * 24 * 60 * 60));
+    ledgerAdapter.exerciseChoice(
+        INTERMEDIARY_PARTY, clientTradeCid.exerciseLifecycle(marketData2019Cid));
+    ledgerAdapter.setCurrentTime(Instant.ofEpochSecond(date2.toEpochDay() * 24 * 60 * 60));
 
     assertTrue(
-        sandbox.observeMatchingContracts(
+        ledgerAdapter.observeMatchingContracts(
             INTERMEDIARY_PARTY,
             PaymentInstructions.TEMPLATE_ID,
             PaymentInstructions::fromValue,
             false,
             i -> i.transactionReference.equals("CLIENT001")));
     assertTrue(
-        sandbox.observeMatchingContracts(
+        ledgerAdapter.observeMatchingContracts(
             INTERMEDIARY_PARTY,
             CouponEvent.TEMPLATE_ID,
             CouponEvent::fromValue,
@@ -151,25 +142,22 @@ public class StructuredProductsIT {
             e -> e.tradeId.equals("CLIENT001")));
 
     // Knockout Event: Issuer vs. Intermediary
-    sandbox
-        .getLedgerAdapter()
-        .exerciseChoice(ISSUER_PARTY, tradeCid.exerciseLifecycle(marketData2022Cid));
-    sandbox.getCreatedContractId(
+    ledgerAdapter.exerciseChoice(ISSUER_PARTY, tradeCid.exerciseLifecycle(marketData2022Cid));
+    ledgerAdapter.getCreatedContractId(
         ISSUER_PARTY, KnockOutEvent.TEMPLATE_ID, KnockOutEvent.ContractId::new);
 
     // Knockout Event: Intermediary vs. Client
-    sandbox
-        .getLedgerAdapter()
-        .exerciseChoice(INTERMEDIARY_PARTY, clientTradeCid.exerciseLifecycle(marketData2022Cid));
+    ledgerAdapter.exerciseChoice(
+        INTERMEDIARY_PARTY, clientTradeCid.exerciseLifecycle(marketData2022Cid));
     assertTrue(
-        sandbox.observeMatchingContracts(
+        ledgerAdapter.observeMatchingContracts(
             INTERMEDIARY_PARTY,
             KnockOutEvent.TEMPLATE_ID,
             KnockOutEvent::fromValue,
             false,
             e -> e.tradeId.equals("CLIENT001")));
     assertTrue(
-        sandbox.observeMatchingContracts(
+        ledgerAdapter.observeMatchingContracts(
             INTERMEDIARY_PARTY,
             PaymentInstructions.TEMPLATE_ID,
             PaymentInstructions::fromValue,
@@ -185,13 +173,14 @@ public class StructuredProductsIT {
   }
 
   private <Cid, Contract> Cid find(
+      DefaultLedgerAdapter ledgerAdapter,
       Party party,
       Identifier id,
       Function<String, Cid> idFactory,
       Function<Value, Contract> ctor,
       Predicate<Contract> predicate) {
     while (true) {
-      ContractWithId<Cid> matchedContract = sandbox.getMatchedContract(party, id, idFactory);
+      ContractWithId<Cid> matchedContract = ledgerAdapter.getMatchedContract(party, id, idFactory);
       Contract contract = ctor.apply(matchedContract.record);
       if (predicate.test(contract)) {
         return matchedContract.contractId;
